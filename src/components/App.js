56d899fe06616a5e7665358431849bf78b7e7756
sbalayan1 <balayans2014@yahoo.com>
2021-06-09 16:42:12 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,20 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [mode, setMode] = useState(false)
+
+  let changeMode = mode ? true : false
 
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
+
+  //you are able to adjust the div element by changing the conditional in the ternary function to a separate function that changes based on its state. 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (changeMode ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={()=> setMode(!mode)}>Change Mode</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,29 @@
-import React from "react";
+import {React, useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+    let [category, setCategory] = useState("All")
+
+    //if we were to extrapolate setCategory(e.target.value), 
+    //what it does is set the current state to the value that we are targeting when we change the selector 
+    //so for instance onChange -> if you select Produce, category = Produce
+
+    let filterItems = items.filter((item)=> {
+        if (category === "All") {
+            return true
+        } else {
+            return category === item.category
+
+        }
+    })
+
+    //filterItems essentially returns either the entire items array, or a new array whose category within each element is equal to the current state of category.
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e) => setCategory(e.target.value)}> 
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +31,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filterItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
